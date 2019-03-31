61ad2f490b3df7e3aa94f2aac8eb783187699f0e
Laura Murphy <laurahelenamurphy@gmail.com>
2019-03-31 14:50:53 +0100

    Made aliens go down and left once only

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ document.addEventListener('DOMContentLoaded', () => {
   const width = 16
   const height = 8
   const squares = []
-  let bullet = 0
   let playerIndex = 120
-  let bulletLocation = [playerIndex - width]
+  //let alienIndex = 0
+  //const bulletLocation = [playerIndex - width]
+
+  //let testAliens = [54, 55, 56]
 
   let aliens = [
     0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
@@ -71,18 +73,92 @@ document.addEventListener('DOMContentLoaded', () => {
     squares[alienIndex].classList.add('alien')
   })
 
-  setInterval(() => {
+  function aliensRight() {
+    // loop over aliens array and remove alien class from current squares
     aliens.forEach(alienIndex => {
       squares[alienIndex].classList.remove('alien')
     })
-
+    // update array with plus 1 index
     aliens = aliens.map(alienIndex => alienIndex + 1)
 
+    // loop over array again and add class of alien to new squares
     aliens.forEach(alienIndex => {
       squares[alienIndex].classList.add('alien')
     })
-  }, 500)
+
+
+  }
+
+  function aliensLeft() {
+
+    // loop over aliens array and remove alien class from current squares
+    aliens.forEach(alienIndex => {
+      squares[alienIndex].classList.remove('alien')
+    })
+    // update array with plus 1 index
+    aliens = aliens.map(alienIndex => alienIndex - 1)
+
+    // loop over array again and add class of alien to new squares
+    aliens.forEach(alienIndex => {
+      squares[alienIndex].classList.add('alien')
+    })
+  }
+
+  var interval = setInterval(() => {
+    aliensRight()
+    const lastAlienIndex = aliens[0]
+    if(
+      aliens[lastAlienIndex] === 10
+    ) {
+
+      aliens.forEach(alienIndex => {
+        squares[alienIndex].classList.remove('alien')
+      })
+      aliens = aliens.map(alienIndex => alienIndex + 16)
+
+
+      clearInterval(interval)
+
+      aliensLeft()
+      setInterval(aliensLeft, 1000)
+      // aliens.forEach(alienIndex => {
+      //   squares[alienIndex].classList.add('alien')
+      // })
+      console.log(aliensLeft)
+      console.log(aliens)
+    }
+
+    // when alien array reaches width-1 (15), need to plus 16 to whole array
+    // then need to move aliens alienIndex-- each time
+
+  }, 1000)
+
+
+
+  // //=========TEST ALIENS=========
+  //
+  // testAliens.forEach(testAlienIndex => {
+  //   squares[testAlienIndex].classList.add('alien')
+  // })
+  //
+  // setInterval(() => {
+  //   testAliens.forEach(testAlienIndex => {
+  //     squares[testAlienIndex].classList.remove('alien')
+  //   })
+  //   testAliens = testAliens.map(testAlienIndex => testAlienIndex - 1)
+  //
+  //   // loop over array again and add class of alien to new squares
+  //   testAliens.forEach(testAlienIndex => {
+  //     squares[testAlienIndex].classList.add('alien')
+  //   })
+  // }, 200)
 
 
 // DO NOT REMOVE BRACKETS BELOW
 })
+
+
+// so stuck on how to use the updated map array the then move all the alies down a row and then move them back across the other way
+// first approach to make aliens go backwards was to test them from startign point which meant they were going into negatives in the array which came back as undefined so that was unehlpful.
+// putting all the functions within one setinterval is not working
+//
